76c5014376c20c6587c7225f34e274a9d35525d0
ThaiPDq <thai.phamcpe@hcmut.edu.vn>
2025-08-11 21:25:54 +0700

    add TextBuffer & Text Buffer

--- RopeTextBuffer.h
+++ RopeTextBuffer.h
@@ -7,7 +7,71 @@
 /*
     Student can define other list data structures here
 */
+template <typename T>
+class DoublyLinkedList {
+    // TODO: may provide some attributes
 
+public:
+    DoublyLinkedList();
+    ~DoublyLinkedList();
+
+    void insertAtHead(T data);
+    void insertAtTail(T data);
+    void insertAt(int index, T data);
+    void deleteAt(int index);
+    T& get(int index) const;
+    int indexOf(T item) const;
+    bool contains(T item) const;
+    int size() const;
+    void reverse();
+    string toString(string (*convert2str)(T&) = 0) const;
+};
+
+class TextBuffer {
+private:
+    DoublyLinkedList<char> buffer; 
+    int cursorPos;
+
+    // TODO: may provide some attributes
+
+public:
+    TextBuffer();
+    ~TextBuffer();
+
+    void insert(char c);
+    void deleteChar();
+    void moveCursorLeft();
+    void moveCursorRight();
+    void moveCursorTo(int index);
+    string getContent() const;
+    int getCursorPos() const;
+    int findFirstOccurrence(char c) const;
+    int* findAllOccurrences(char c, int &count) const;
+    void sortAscending();
+    void deleteAllOccurrences(char c);
+    void undo();
+    void redo();
+    void printHistory() const {
+        historyManager->printHistory();
+    }
+
+public:
+    class HistoryManager {
+        // TODO: may provide some attributes
+
+    public:
+        HistoryManager();
+        ~HistoryManager();
+
+        void addAction(const string &actionName, int cursorPos, char c);
+        void printHistory() const;
+        int size() const;
+    };
+
+private:
+    // History manager instance
+    HistoryManager* historyManager;
+};
 /**
  * Rope (AVL-based, fixed leaf chunk size = 8)
  */
