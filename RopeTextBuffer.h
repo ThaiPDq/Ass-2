6ce4e947e9f4461221423e8ef0fb980949a9a0a5
ThaiPDq <thai.phamcpe@hcmut.edu.vn>
2025-08-12 13:25:56 +0700

    adding part of part 1

--- RopeTextBuffer.h
+++ RopeTextBuffer.h
@@ -7,108 +7,57 @@
 /*
     Student can define other list data structures here
 */
-template <typename T>
-class DoublyLinkedList {
-    // TODO: may provide some attributes
 
-public:
-    DoublyLinkedList();
-    ~DoublyLinkedList();
-
-    void insertAtHead(T data);
-    void insertAtTail(T data);
-    void insertAt(int index, T data);
-    void deleteAt(int index);
-    T& get(int index) const;
-    int indexOf(T item) const;
-    bool contains(T item) const;
-    int size() const;
-    void reverse();
-    string toString(string (*convert2str)(T&) = 0) const;
-};
-
-class TextBuffer {
-private:
-    DoublyLinkedList<char> buffer; 
-    int cursorPos;
-
-    // TODO: may provide some attributes
-
-public:
-    TextBuffer();
-    ~TextBuffer();
-
-    void insert(char c);
-    void deleteChar();
-    void moveCursorLeft();
-    void moveCursorRight();
-    void moveCursorTo(int index);
-    string getContent() const;
-    int getCursorPos() const;
-    int findFirstOccurrence(char c) const;
-    int* findAllOccurrences(char c, int &count) const;
-    void sortAscending();
-    void deleteAllOccurrences(char c);
-    void undo();
-    void redo();
-    void printHistory() const {
-        historyManager->printHistory();
-    }
-
-public:
-    class HistoryManager {
-        // TODO: may provide some attributes
-
-    public:
-        HistoryManager();
-        ~HistoryManager();
-
-        void addAction(const string &actionName, int cursorPos, char c);
-        void printHistory() const;
-        int size() const;
-    };
-
-private:
-    // History manager instance
-    HistoryManager* historyManager;
-};
 /**
  * Rope (AVL-based, fixed leaf chunk size = 8)
  */
-class Rope {
+class Rope
+{
 public:
     static const int CHUNK_SIZE = 8;
+
 private:
-    class Node {
+    class Node
+    {
     public:
-        enum BalanceFactor { LH = 1, EH = 0, RH = -1 };
+        enum BalanceFactor
+        {
+            LH = 1,
+            EH = 0,
+            RH = -1
+        };
+        friend class Rope;
 
     private:
-        Node* left;
-        Node* right;
+        Node *left;
+        Node *right;
         string data;
         int weight;
         int height;
         BalanceFactor balance;
 
         Node();
-        explicit Node(const string& s);
+        explicit Node(const string &s);
         bool isLeaf() const;
+        friend class TestHelper;
     };
 
-    Node* root;
+    Node *root;
 
-    int height(Node* node) const;
-    int getTotalLength(Node* node) const;
-    void update(Node* node);
-    Node* rotateLeft(Node* x);
-    Node* rotateRight(Node* y);
-    Node* rebalance(Node* node);
-    void split(Node* node, int index, Node*& outLeft, Node*& outRight);
-    Node* concatNodes(Node* left, Node* right);
-    char charAt(Node* node, int index) const;
-    string toString(Node* node) const;
-    void destroy(Node*& node);
+    int height(Node *node) const;
+    int getTotalLength(Node *node) const;
+    void update(Node *node);
+    Node *rotateLeft(Node *x);
+    Node *rotateRight(Node *y);
+    Node *rebalance(Node *node);
+    void split(Node *node, int index, Node *&outLeft, Node *&outRight);
+    Node *concatNodes(Node *left, Node *right);
+    char charAt(Node *node, int index) const;
+    string toString(Node *node) const;
+    void destroy(Node *&node);
+
+    // Helper
+    string substringHelper(Node *node, int start, int end) const;
 
 public:
     Rope();
@@ -118,14 +67,17 @@ public:
     bool empty() const;
     char charAt(int index) const;
     string substring(int start, int length) const;
-    void insert(int index, const string& s);
+    void insert(int index, const string &s);
     void deleteRange(int start, int length);
     string toString() const;
 
+    friend class TestHelper;
 #ifdef TESTING
     friend class TestHelper;
 #endif
 };
+
+int max(int a, int b);
 
 class RopeTextBuffer {
 public:
